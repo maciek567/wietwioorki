d6e92e9f502140300d135f83580028e5b7c08dfb
MaciejBanaś <maciejb.98@gmail.com>
2019-11-25 20:59:09 +0100

    Main view

--- src/main/java/pl/wietwioorki/to2019/controller/HomeSceneController.java
+++ src/main/java/pl/wietwioorki/to2019/controller/HomeSceneController.java
@@ -1,11 +1,25 @@
 package pl.wietwioorki.to2019.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class HomeSceneController {
     @FXML
-    private Label label;
+    private Button reader;
+
+    @FXML
+    private Button book;
+
+    @FXML
+    public void handleNewReader(ActionEvent actionEvent) {
+        System.out.println("New reader added");
+    }
+
+    @FXML
+    public void handleNewBook(ActionEvent actionEvent) {
+        System.out.println("New book added");
+    }
 }
