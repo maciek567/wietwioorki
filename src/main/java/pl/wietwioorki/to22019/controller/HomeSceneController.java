d7d0561ff721dd2bd5d1ce158ba608c773efcde3
hubert-guzowski <hubert.guzowski@gmail.com>
2019-11-29 09:37:45 +0100

    Merge branch 'm1' of C:\Users\Hubert\IdeaProjects\librarySystem with conflicts.

--- src/main/java/pl/wietwioorki/to22019/controller/HomeSceneController.java
+++ src/main/java/pl/wietwioorki/to22019/controller/HomeSceneController.java
@@ -3,9 +3,13 @@ package pl.wietwioorki.to22019.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
 
 @Controller
 //@Component
@@ -35,18 +39,18 @@ public class HomeSceneController {
     @FXML
     public void handleNewBook(ActionEvent actionEvent) {
         System.out.println("New book view");
-//        fxmlLoader.setLocation(getClass().getResource("/layouts/AddBook.fxml"));
-//        Parent rootNode = null;
-//
-//        try {
-//            rootNode = fxmlLoader.load();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        Scene scene = new Scene(rootNode, 800, 600);
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
+        fxmlLoader.setLocation(getClass().getResource("/layouts/AddBook.fxml"));
+        Parent rootNode = null;
+
+        try {
+            rootNode = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scene scene = new Scene(rootNode, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
 }
