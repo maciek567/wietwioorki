0838bc72c53267efa3b4f2486b0bf7d7a583ede2
Wicz3k <wilk.s98@wp.pl>
2019-12-13 11:30:46 +0100

    filter on reservations

--- src/main/java/pl/wietwioorki/to22019/controller/ReservationListController.java
+++ src/main/java/pl/wietwioorki/to22019/controller/ReservationListController.java
@@ -1,8 +1,10 @@
 package pl.wietwioorki.to22019.controller;
 
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,14 +46,32 @@ public class ReservationListController {
     private TableColumn<Reservation, Date> returnDate;
 
     @FXML
-    private void initialize() {
-        reservationTable.setItems(ReservationDAO.getReservationsObservable());
+    private TextField filterField;
 
+    @FXML
+    private void initialize() {
         reservationId.setCellValueFactory(dataValue -> dataValue.getValue().getReservationIdProperty());
         readerPesel.setCellValueFactory(dataValue -> dataValue.getValue().getReaderPeselProperty());
         readerName.setCellValueFactory(dataValue -> dataValue.getValue().getReaderNameProperty());
         booksTittle.setCellValueFactory(dataValue -> dataValue.getValue().getBooksTittleProperty());
         borrowingDate.setCellValueFactory(dataValue -> dataValue.getValue().getBorrowingDateProperty());
         returnDate.setCellValueFactory(dataValue -> dataValue.getValue().getReturnDateProperty());
+
+        FilteredList<Reservation> filteredData = new FilteredList<>(ReservationDAO.getReservationsObservable(), p -> true);
+
+        filterField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(reservation -> {
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+                String caseFilter = newValue;
+                if (reservation.getReaderPeselProperty().getValue().toString().startsWith(caseFilter)) {
+                    return true;
+                }
+                return false;
+            });
+        });
+
+        reservationTable.setItems(filteredData);
     }
 }
