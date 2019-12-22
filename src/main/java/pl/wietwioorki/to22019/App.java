30b11586b44060beb64afcb2ad6e9cbf1ca2e0da
moniad <moni.dziedzic@gmail.com>
2019-12-22 18:08:51 +0100

    Initial commit to m3

--- src/main/java/pl/wietwioorki/to22019/App.java
+++ src/main/java/pl/wietwioorki/to22019/App.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import pl.wietwioorki.to22019.controller.HomeSceneController;
 
 
 @SpringBootApplication
