d5ddd0740ddee2bb36f594cb55c95918b2914417
hubert-guzowski <hubert.guzowski@gmail.com>
2020-01-19 20:10:19 +0100

    fixed profile editing. Tweaked HomeScene view

--- src/main/java/pl/wietwioorki/to22019/controller/EditProfileController.java
+++ src/main/java/pl/wietwioorki/to22019/controller/EditProfileController.java
@@ -7,16 +7,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import org.springframework.stereotype.Controller;
-import pl.wietwioorki.to22019.model.Reader;
-import pl.wietwioorki.to22019.model.ReservationStatus;
-import pl.wietwioorki.to22019.model.Role;
-import pl.wietwioorki.to22019.model.User;
 import pl.wietwioorki.to22019.util.AlertFactory;
 import pl.wietwioorki.to22019.validator.RegistrationValidator;
 
-import java.util.HashMap;
-
-import static pl.wietwioorki.to22019.util.InfoMessage.readerSuccessfullyCreatedContent;
+import static pl.wietwioorki.to22019.util.InfoMessage.readerSuccessfullyEditedContent;
 import static pl.wietwioorki.to22019.util.InfoMessage.successHeader;
 
 @Controller
@@ -25,57 +19,44 @@ public class EditProfileController extends AbstractWindowController{
     public TextField name;
 
     @FXML
-    public TextField pesel;
-
-    @FXML
-    public TextField login;
-
-    @FXML
     public TextField email;
 
     @FXML
-    public PasswordField registrationPassword;
+    private PasswordField newPassword;
 
     @FXML
-    public PasswordField passwordConfirmation;
+    private PasswordField newPasswordConfirmation;
 
     @FXML
     public Button confirmation;
+
+    @FXML
+    private void initialize() {
+        name.setText(sessionConstants.getCurrentReader().getFullName());
+        email.setText(sessionConstants.getCurrentUser().getEmail());
+    }
 
     @FXML
     public void handleEdit(ActionEvent actionEvent) {
         System.out.println("Editing reader");
 
         RegistrationValidator registrationValidator = sessionConstants.getRegistrationValidator();
-        if (!registrationValidator.validateName(name.getText()) || !registrationValidator.validatePesel(pesel.getText()) ||
-                !registrationValidator.validateUser(login.getText()) ||
-                !registrationValidator.validateEmail(email.getText()) ||
-                !registrationValidator.validatePasswords(registrationPassword.getText(), passwordConfirmation.getText())) {
+        if (!registrationValidator.validateName(name.getText()) || !registrationValidator.validateEmail(email.getText()) ||
+                !registrationValidator.validatePasswords(newPassword.getText(), newPasswordConfirmation.getText())) {
             return;
         }
 
-        Long peselNumber = Long.parseLong(pesel.getText());
+        sessionConstants.getReaderRepository().updateFullName(sessionConstants.getCurrentReader().getPesel(),
+                name.getText());
 
-        Reader reader = sessionConstants.getCurrentReader();
-        User user = sessionConstants.getCurrentUser();
+        sessionConstants.getUserRepository().updateEmail(sessionConstants.getCurrentUser().getLogin(),
+                email.getText());
+
+        sessionConstants.getUserRepository().updatePassword(sessionConstants.getCurrentUser().getLogin(),
+                newPassword.getText());
 
 
-        sessionConstants.getReaderRepository().delete(reader);
-        sessionConstants.getUserRepository().delete(user);
-
-        sessionConstants.getReaderRepository().flush();
-        sessionConstants.getUserRepository().flush();
-
-        reader.setFullName(name.getText());
-        reader.setPesel(peselNumber);
-        user.setLogin(login.getText());
-        user.setEmail(email.getText());
-        user.setPassword(registrationPassword.getText());
-
-        reader.setUser(user);
-        sessionConstants.getUserRepository().save(user);
-        AlertFactory.showAlert(Alert.AlertType.INFORMATION, successHeader, readerSuccessfullyCreatedContent);
-
+        AlertFactory.showAlert(Alert.AlertType.INFORMATION, successHeader, readerSuccessfullyEditedContent);
         closeWindowAfterSuccessfulAction(actionEvent);
     }
 }
--- src/main/java/pl/wietwioorki/to22019/repository/ReaderRepository.java
+++ src/main/java/pl/wietwioorki/to22019/repository/ReaderRepository.java
@@ -1,10 +1,20 @@
 package pl.wietwioorki.to22019.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.wietwioorki.to22019.model.Reader;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface ReaderRepository extends JpaRepository<Reader, Long> {
 //    Reader findByFullName(String fullName);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Reader c SET c.fullName = :fullName WHERE c.pesel = :pesel")
+    void updateFullName(@Param("pesel") long pesel, @Param("fullName") String fullName);
 }
